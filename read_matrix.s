00306623b50ca657595406e7ab3f91a655e6ffd0
Class Account <cs61c-bsl@hive26.CS.Berkeley.EDU>
2019-10-21 00:05:10 -0700

    Task1 passed test

--- read_matrix.s
+++ read_matrix.s
@@ -19,17 +19,82 @@
 read_matrix:
 
     # Prologue
-	
+    addi sp, sp, -32
+    sw s0, 0(sp)                # the pointer to string representing the filename
+    sw s1, 4(sp)                # a pointer to the number of rows
+    sw s2, 8(sp)                # a pointer to the number of columns
+    sw s3, 12(sp)               # the file descriptor
+    sw s4, 16(sp)               # the pointer to the matrix in memory
+    sw s5, 20(sp)               # save the # of entries
+    sw s6, 24(sp)               # counter for entries
+    sw ra, 28(sp)
 
+    add s0, a0, x0
+    add s1, a1, x0
+    add s2, a2, x0
 
+    add a1, s0, x0              # fopen("filename", "-r")
+    add a2, x0, x0
+    call fopen
+    addi t0, x0, -1
+    beq t0, a0, eof_or_error    # if file descriptor (fp) is -1: exit with exit code 1
 
+    add s3, a0, x0
+    
+    add a1, s3, x0
+    add a2, s1, x0
+    addi a3, x0, 4
+    call fread                  # fread(fp, row, 4)
+    bne a0, a3, eof_or_error    # if a0 is NOT equal to a3: exit with exit code 1
 
+    add a1, s3, x0
+    add a2, s2, x0
+    addi a3, x0, 4
+    call fread                  # fread(fp, column, 4)
+    bne a0, a3, eof_or_error    # if a0 is NOT equal to a3: exit with exit code 1
 
+    lw t0, 0(s1)                # t0 <- row
+    lw t1, 0(s2)                # t1 <- column
+    mul a0, t0, t1              # a0 <- t0 * t1 (# of entries)
+    add s5, a0, x0              # save the # of entries
+    addi t0, x0, 4
+    mul a0, a0, t0
+    call malloc
+    beq a0, x0, eof_or_error    # if a pointer returned is NULL: exit with exit code 1
 
+    add s4, a0, x0              # save the returned pointer
 
+    add s6, x0, x0              # counter for entries
+read_entry:
+    bgeu s6, s5, finish
+    add a1, s3, x0
+    addi t0, x0, 4
+    mul t0, t0, s6
+    add a2, t0, x0
+    add a2, a2, s4
+    addi a3, x0, 4
+    call fread
+    bne a0, a3, eof_or_error
+    addi s6, s6, 1
+    j read_entry
+
+finish:
+    add a1, s3, x0              # fclose(fp)
+    call fclose
+    bne a0, x0, eof_or_error
+
+    add a0, s4, x0
 
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw s6, 24(sp)
+    lw ra, 28(sp)
+    addi sp, sp, 32
 
     ret
 
--- test_files/test_read_matrix.s
+++ test_files/test_read_matrix.s
@@ -7,11 +7,61 @@ file_path: .asciiz "./test_input.bin"
 .text
 main:
     # Read matrix into memory
+    addi a0, x0, 4
+    call malloc
+    add s0, a0, x0                    # rows of matrix
 
+    addi a0, x0, 4
+    call malloc
+    add s1, a0, x0                    # columns of matrix
+
+    la a0, file_path
+    add a1, s0, x0
+    add a2, s1, x0
+    call read_matrix
+    add s5, a0, x0
 
     # Print out elements of matrix
+    lw a1, 0(s0)
+    call print_int
+    addi a1, x0, ' '
+    call print_char
+    lw a1, 0(s1)
+    call print_int
+    addi a1, x0, '\n'
+    call print_char
 
+    add s2, x0, x0                    # counter of outer loop
+print_outer_loop:
+    lw t0, 0(s0)
+    bge s2, t0, finish
+    lw t0, 0(s1)
+    mul t0, s2, t0
+    addi t1, x0, 4
+    mul s4, t0, t1                    # the address of the first entry in some row
+    add s4, s5, s4
+    add s3, x0, x0                    # counter of inner loop
 
+print_inner_loop:
+    lw t0, 0(s1)
+    bge s3, t0, finish_inner_loop
+    addi t1, x0, 4
+    mul t0, s3, t1
+    add t0, s4, t0
+    lw a1, 0(t0)
+    call print_int
+    addi a1, x0, ' '
+    call print_char
+    addi s3, s3, 1
+    j print_inner_loop
+
+finish_inner_loop:
+    addi a1, x0, '\n'
+    call print_char
+    addi s2, s2, 1
+    j print_outer_loop
+
+finish:
     # Terminate the program
     addi a0, x0, 10
     ecall
